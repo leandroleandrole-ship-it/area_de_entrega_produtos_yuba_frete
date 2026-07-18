ebcb48d8bff707183595790324a35048aa13b781
leandroleandrole-ship-it <leandroleandrole@gmail.com>
2026-07-18 02:02:23 -0300

    Clean up js/config.js formatting
    Removed unnecessary newlines in the configuration object.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window.YUBA_CONFIG = {
   SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co",
-
   SUPABASE_ANON_KEY: "sb_publishable_jYUJW4hr8eN1q1TaQrBIKw_wzt306mj",
-  
+
   DISTRIBUTION_CENTER: {
     name: "Centro de Distribuição Produtos Yuba",
     address: "São Paulo/SP",
