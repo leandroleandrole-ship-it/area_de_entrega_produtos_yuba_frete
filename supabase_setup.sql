a78a7f30a69968e044becaba366c015a25f1ef08
leandroleandrole-ship-it <leandroleandrole@gmail.com>
2026-07-17 21:49:13 -0300

    Change Supabase anon key in configuration
    Updated the Supabase anon key for authentication.

--- supabase_setup.sql
+++ supabase_setup.sql
@@ -1,6 +1,6 @@
 window.YUBA_CONFIG = {
     SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co/rest/v1/",
-    SUPABASE_ANON_KEY: "sb_publishable_jYUJW4hr8eN1q1TaQrBIKw_wzt306mj",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wb2pvam1ybXNjYWN6bWZzcW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMTg5NDAsImV4cCI6MjA5OTg5NDk0MH0._AJ8oJ9ayRquU3UV-4zvQZdN4IRwJfVFSRSKLiab2QU",
   DISTRIBUTION_CENTER: {
     name: "Centro de Distribuição Produtos Yuba",
     address: "São Paulo/SP",
