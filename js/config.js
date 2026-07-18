b194f0d1a80c2b703d2b6a5d092d60c605a683f7
leandroleandrole-ship-it <leandroleandrole@gmail.com>
2026-07-17 21:00:30 -0300

    Modify SUPABASE_URL in config.js
    Updated SUPABASE_URL to remove unnecessary path.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.YUBA_CONFIG = {
-    SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co/rest/v1/",
+    SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co",
     SUPABASE_ANON_KEY: "COLsb_publishable_jYUJW4hr8eN1q1TaQrBIKw_wzt306mj",
   DISTRIBUTION_CENTER: {
     name: "Centro de Distribuição Produtos Yuba",
