b4f30596a00debf75f0930ca121e0cea81b33ada
leandroleandrole-ship-it <leandroleandrole@gmail.com>
2026-07-17 21:07:40 -0300

    Update SUPABASE_ANON_KEY in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.YUBA_CONFIG = {
     SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co",
-    SUPABASE_ANON_KEY: "COLsb_publishable_jYUJW4hr8eN1q1TaQrBIKw_wzt306mj",
+    SUPABASE_ANON_KEY: "sb_publishable_jYUJW4hr8eN1q1TaQrBIKw_wzt306mj",
   DISTRIBUTION_CENTER: {
     name: "Centro de Distribuição Produtos Yuba",
     address: "São Paulo/SP",
