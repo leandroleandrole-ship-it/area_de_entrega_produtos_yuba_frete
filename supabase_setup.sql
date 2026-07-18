329230dc7378e594d8c9fadc7f98b82fcf9cdc99
leandroleandrole-ship-it <leandroleandrole@gmail.com>
2026-07-17 21:50:37 -0300

    Update Supabase URL in configuration

--- supabase_setup.sql
+++ supabase_setup.sql
@@ -1,5 +1,5 @@
 window.YUBA_CONFIG = {
-    SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co/rest/v1/",
+    SUPABASE_URL: "https://opojojmrmscaczmfsqol.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wb2pvam1ybXNjYWN6bWZzcW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMTg5NDAsImV4cCI6MjA5OTg5NDk0MH0._AJ8oJ9ayRquU3UV-4zvQZdN4IRwJfVFSRSKLiab2QU",
   DISTRIBUTION_CENTER: {
     name: "Centro de Distribuição Produtos Yuba",
